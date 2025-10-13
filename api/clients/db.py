e2f4723a0e31640d4a1e2b9a9141e399de56aea5
Ritwik Raj <ritwikr@sg360.com>
2025-10-13 18:10:08 +0530

    Restructure API

--- api/clients/db.py
+++ api/clients/db.py
@@ -5,4 +5,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 async def get_db_pool():
-    return await asyncpg.create_pool(DATABASE_URL)
+    return await asyncpg.create_pool(DATABASE_URL, statement_cache_size=0)
